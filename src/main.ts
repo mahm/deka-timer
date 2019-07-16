07cd5e76f904c46bc31aec9e3ae2bc30f816a09d
Masahiro Nishimi <nishimi@sonicgarden.jp>
2019-07-16 11:26:40 +0900

    feat: PWA設定を追加

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import Vue from 'vue';
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
-import './font.scss';
-import App from './App.vue';
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import "./font.scss";
+import App from "./App.vue";
+import "./registerServiceWorker";
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
